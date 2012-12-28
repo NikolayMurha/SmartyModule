33ef529d5927ed78a23cdc9af3e906e01aae1ce9
Murga Nikolay <work@murga.kiev.ua>
2012-12-28 17:13:48 +0200

    Add factory for template path stak resolver

---
+++ src/SmartyModule/Service/ViewTemplatePathStackFactory.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nikolay
+ * Date: 28.12.12
+ * Time: 14:58
+ * To change this template use File | Settings | File Templates.
+ */ 
+class TemplatePathStack {
+
+}
