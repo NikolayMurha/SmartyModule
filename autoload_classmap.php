af028297ed24861f3c5f7f08884fedabb532823e
Murga Nikolay <work@murga.kiev.ua>
2012-12-28 17:34:22 +0200

    change class map

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -6,9 +6,8 @@
  * @package     SmartyModule
  */
 return array(
-    'Smarty' => __DIR__ . '/vendor/Smarty/Smarty.class.php',
-    'SmartyBC' => __DIR__ . '/vendor/Smarty/SmartyBC.class.php',
     'SmartyModule\View\Renderer\SmartyRenderer' => __DIR__ . '/src/SmartyModule/View/Renderer/SmartyRenderer.php',
     'SmartyModule\View\Strategy\SmartyStrategy' => __DIR__ . '/src/SmartyModule/View/Strategy/SmartyStrategy.php',
+    'SmartyModule\Service\ViewTemplatePathStackFactory' => __DIR__ . '/src/SmartyModule/Service/ViewTemplatePathStackFactory.php',
     'SmartyModule\Module' => __DIR__ . '/Module.php',
 );
