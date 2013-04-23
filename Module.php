fb7271bc30600828d245436742b629cdf17748b6
Max Tsepkov <max@garygolden.me>
2013-04-24 00:35:13 +0400

    set up autoloading for both smarty and module

--- Module.php
+++ Module.php
@@ -1,14 +1,31 @@
 <?php
-
 /**
  * @link        https://github.com/MurgaNikolay/SmartyModule for the canonical source repository
  * @license     http://framework.zend.com/license/new-bsd New BSD License
  * @author      Murga Nikolay <work@murga.kiev.ua>
  * @package     SmartyModule
- *
- * This file is placed here for compatibility with Zendframework 2's ModuleManager.
- * It allows usage of this module even without composer.
- * The original Module.php is in 'src/SmartyModule' in order to respect PSR-0
  */
+namespace SmartyModule;
 
-require_once __DIR__ . '/src/SmartyModule/Module.php';
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        $config = include __DIR__ . '/config/module.config.php';
+        return $config;
+    }
+}
