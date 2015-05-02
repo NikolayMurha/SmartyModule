3ba5669710133284af3ed21c3f5e5d87e5f664d7
Matthias Glaub <magl@magl.net>
2015-05-02 16:43:00 +0200

    switch to psr-4 autoloading use composers autoloading possibilities instead of zend framework autoloader

--- autoload_register.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @link        https://github.com/MurgaNikolay/SmartyModule for the canonical source repository
- * @license     http://framework.zend.com/license/new-bsd New BSD License
- * @author      Murga Nikolay <work@murga.kiev.ua>
- * @package     SmartyModule
- */
-spl_autoload_register(include __DIR__ . '/autoload_function.php');
---
+++ src/SmartyModule/Module.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link        https://github.com/MurgaNikolay/SmartyModule for the canonical source repository
+ * @license     http://framework.zend.com/license/new-bsd New BSD License
+ * @author      Murga Nikolay <work@murga.kiev.ua>
+ * @package     SmartyModule
+ */
+namespace SmartyModule;
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        $config = include __DIR__ . '/../../config/module.config.php';
+        return $config;
+    }
+}
